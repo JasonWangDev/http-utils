1a33ca2512491cdf65c4fe8f23f4aec46fcd87da
DOITSKY\jason <jasonwang@doitsky.com>
2017-05-22 16:45:39 +0800

    封裝部分 HTTP 方法

--- app/src/main/java/idv/jasonwang/httputils/sample/SampleActivity.java
+++ app/src/main/java/idv/jasonwang/httputils/sample/SampleActivity.java
@@ -1,9 +1,11 @@
 package idv.jasonwang.httputils.sample;
 
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -16,17 +18,19 @@ public class SampleActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sample);
 
+        final HttpClient httpClient = HttpClient.getInstance();
+
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                HttpClient httpClient = HttpClient.getInstance();
+                Map<String, String> login = new HashMap<>();
+                login.put("X", "X");
+                login.put("X", "X");
+                login.put("X", "X");
 
-                Map<String, String> params = new HashMap<>();
-                params.put("user_account", "test001@salesky.net");
-                params.put("user_passwd", "doit123456");
-                params.put("product_code", "salesky");
+                httpClient.post("X", login);
 
-                httpClient.post("https://dev.sky-family.net/api/M_SaleSky_Login.php", params);
+
 
 
 //                File file = new File(Environment.getExternalStorageDirectory() + "/download/LARGE_elevation.jpg");
@@ -36,11 +40,23 @@ public class SampleActivity extends AppCompatActivity {
 //                } catch (FileNotFoundException e) {
 //                    e.printStackTrace();
 //                }
-
+//
 //                new HttpUtils().postMultiPart(inputStream);
 //                new HttpUtils().postMultiPartWithProgress(file);
 
 //                new HttpUtils().downloadFileWithProgress(Environment.getExternalStorageDirectory() + "/download", "Download_Test.jpg");
+            }
+        });
+
+        findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Map<String, Object> params = new HashMap<>();
+                params.put("X", new File(Environment.getExternalStorageDirectory() + "/download/LARGE_elevation.jpg"));
+                params.put("X", "X");
+                params.put("X", "X");
+
+                httpClient.upload("X", params);
             }
         });
     }
--- http-utils-library-android/src/main/java/idv/jasonwang/httputils/HttpClient.java
+++ http-utils-library-android/src/main/java/idv/jasonwang/httputils/HttpClient.java
@@ -1,19 +1,31 @@
 package idv.jasonwang.httputils;
 
+import android.net.Uri;
 import android.util.Log;
+import android.webkit.MimeTypeMap;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import okhttp3.Call;
 import okhttp3.Callback;
+import okhttp3.Cookie;
+import okhttp3.CookieJar;
 import okhttp3.FormBody;
 import okhttp3.HttpUrl;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
+import okhttp3.logging.HttpLoggingInterceptor;
+import okio.BufferedSink;
 
 /**
  * Created by jason on 2017/5/22.
@@ -26,7 +38,26 @@ public class HttpClient {
 
 
     private HttpClient() {
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+        logging.setLevel(HttpLoggingInterceptor.Level.BASIC);
+
         okHttpClient = new OkHttpClient.Builder()
+                                        .cookieJar(new CookieJar() {
+                                            private final Map<String, List<Cookie>> store = new HashMap<>();
+
+                                            @Override
+                                            public void saveFromResponse(HttpUrl url, List<Cookie> cookies) {
+                                                store.put(url.host(), cookies);
+                                            }
+
+                                            @Override
+                                            public List<Cookie> loadForRequest(HttpUrl url) {
+                                                List<Cookie> cookies = store.get(url.host());
+
+                                                return cookies != null ? cookies : new ArrayList<Cookie>();
+                                            }
+                                        })
+                                        .addNetworkInterceptor(logging)
                                         .build();
     }
 
@@ -132,7 +163,7 @@ public class HttpClient {
         call.enqueue(new Callback() {
             @Override
             public void onFailure(Call call, IOException e) {
-                Log.d("TAG", "onFailure");
+                Log.e("TAG", "onFailure");
                 Log.e("TAG", e.getMessage());
             }
 
@@ -143,12 +174,58 @@ public class HttpClient {
         });
     }
 
-    public void upload(String url, Map<String, String> params, File file) {
-
+    public void upload(String url, Map<String, Object> params) {
+        upload(url, params ,null);
     }
 
-    public void upload(String url, Map<String, String> params, File file, String tag) {
+    public void upload(String url, Map<String, Object> params, String tag) {
+        MultipartBody.Builder form = new MultipartBody.Builder();
+        form.setType(MultipartBody.FORM);
+        if (params != null)
+        {
+            for (String key : params.keySet())
+            {
+                if (!(params.get(key) instanceof File))
+                    form.addFormDataPart(key, (String) params.get(key));
+                else
+                {
+                    File file = (File) params.get(key);
+                    form.addFormDataPart(key, file.getName(), new CountingFileRequestBody(file));
+                }
+            }
+        }
+        RequestBody requestBody = form.build();
 
+        Request.Builder requestBuilder = new Request.Builder();
+        try
+        {
+            requestBuilder.url(url);
+        }
+        catch (IllegalArgumentException e)
+        {
+            Log.e("TAG", "URL Protocol Fail");
+
+            return;
+        }
+        requestBuilder.post(requestBody);
+        if (tag != null)
+            requestBuilder.tag(tag);
+        Request request = requestBuilder.build();
+
+        Call call = okHttpClient.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.e("TAG", "onFailure");
+                Log.e("TAG", e.getMessage());
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                Log.d("TAG", "Done");
+                Log.d("TAG", response.body().string());
+            }
+        });
     }
 
     public void download(String url, File output) {
@@ -178,6 +255,52 @@ public class HttpClient {
             if (tag.equals(call.request().tag()))
                 call.cancel();
         }
+    }
+
+
+    private class CountingFileRequestBody extends RequestBody {
+
+        private final File file;
+
+        public CountingFileRequestBody(File file) {
+            this.file = file;
+        }
+
+        @Override
+        public long contentLength() {
+            return file.length();
+        }
+
+        @Override
+        public MediaType contentType() {
+            String type = null;
+            String extension = MimeTypeMap.getFileExtensionFromUrl(file.getAbsolutePath());
+            if (extension != null)
+                type = MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension);
+
+            return MediaType.parse(type);
+        }
+
+        @Override
+        public void writeTo(BufferedSink sink) throws IOException {
+            FileInputStream inputStream = new FileInputStream(file);
+            long total = 0;
+            int len;
+            byte[] buff = new byte[4096];
+            while ((len = inputStream.read(buff)) != -1)
+            {
+                sink.outputStream().write(buff, 0, len);
+                sink.outputStream().flush();
+
+                total += len;
+
+                Long progress = (long) ((double) total / contentLength() * 100);
+                Log.d("TAG", String.format("Progress: %d", progress));
+            }
+
+            inputStream.close();
+        }
+
     }
 
 }
